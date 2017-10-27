6f4b036a69f364560b659e93c7aea0a453c5455e
Kapil Vaidya <kapilvaidya24@gmail.com>
2017-10-27 23:35:09 +0530

    selectivity value adjusted for apt sized output

--- query_gen_star.py
+++ query_gen_star.py
@@ -1,6 +1,6 @@
 import sys
 import random
-
+import math
 
 # relation size prob.
 # 10-100 15%
@@ -18,29 +18,29 @@ def fsize():
 	t=random.randint(1,101)	
 
 	if(t<16):
-		return random.randint(0,90)+10
+		return (random.randint(0,90)+10)*100
 
 	if(t<46):
-		return random.randint(0,900)+100
+		return (random.randint(0,900)+100)*100
 
 	if(t<71):
-		return random.randint(0,9000)+1000
+		return (random.randint(0,9000)+1000)*100
 
-	return random.randint(0,90000)+10000			
+	return (random.randint(0,90000)+10000)*100			
 
 def dsize():
 	t=random.randint(1,101)	
 
 	if(t<6):
-		return random.randint(0,8)+2
+		return (random.randint(0,8)+2)*100
 
 	if(t<56):
-		return random.randint(0,90)+10
+		return (random.randint(0,90)+10)*100
 
 	if(t<91):
-		return random.randint(0,400)+100
+		return (random.randint(0,400)+100)*100
 
-	return random.randint(0,500)+500			
+	return (random.randint(0,500)+500)*100			
 
 
 
@@ -54,7 +54,7 @@ else:
 centre_size=fsize()
 
 temp=[]
-attr_num=random.randint(1,11)
+attr_num=n-1
 for j in range(0,attr_num):
 	temp.append(dsize())
 
@@ -78,7 +78,7 @@ for i in range(0,n-1):
 for i in range(0,n-1):
 	temp=0.00
 	k=len(centre_dom_size)
-	attr_ind=random.randint(0,k-1)
+	attr_ind=i
 
 	temp=float(centre_dom_size[attr_ind])
 
@@ -89,6 +89,35 @@ for i in range(0,n-1):
 		temp=float(dom_list[i][attr_ind])
 
 	sel_list.append(float(1/temp))	
+
+
+
+
+
+t=float(centre_size)
+
+alpha=1.0
+
+for i in range(0,n-1):
+	t=t*float(size_list[i])
+	t=t*float(sel_list[i])
+
+# print(str(t)+" is value of t")
+
+if(t<1000000 or t>1000000000):
+	t=t/10000000
+	t=1/t
+	alpha=math.log(t,2)
+	alpha=alpha/(n-1)
+	alpha=math.pow(alpha,2)
+
+# print(str(alpha)+" is alpha")
+
+for i in range(1,n):
+	# print(sel_list[i-1])
+	sel_list[i-1]=sel_list[i-1]/alpha	
+	# print(sel_list[i-1])
+
 
 
 
@@ -110,21 +139,19 @@ for i in range(1,n):
 
 	print(sel_list[i-1])
 
-print(centre_size)
+# print(centre_size)
 
-for i in range(0,n-1):
-	print(size_list[i])
+# for i in range(0,n-1):
+# 	print(size_list[i])
+
+# t=float(centre_size)
 
 
-t=float(centre_size)
+# for i in range(0,n-1):
+# 	t=t*float(size_list[i])
+# 	t=t*float(sel_list[i])
 
-alpha=1.0
-
-for i in range(0,n-1):
-	t=t*float(size_list[i])
-	t=t*float(sel_list[i])
-
-print(t)
+# print(t)
 
 
 
